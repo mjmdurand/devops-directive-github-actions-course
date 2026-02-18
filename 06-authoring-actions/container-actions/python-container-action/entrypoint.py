32d0d71062d8ed1e77ca1d82611d896ea848896b
Marc Durand <m.durand@urios.com>
2026-02-18 14:51:46 +0100

    test bye container action

--- 06-authoring-actions/container-actions/python-container-action/entrypoint.py
+++ 06-authoring-actions/container-actions/python-container-action/entrypoint.py
@@ -4,13 +4,17 @@ import os, datetime as dt
 
 # GitHub injects INPUT_WHO_TO_GREET based on the input name
 name = os.getenv("INPUT_WHO_TO_GREET", "World")
+bye_name = os.getenv("INPUT_WHO_TO_BYE", "Lord")
 
 # Alternatively, you could read it from the args list using its position
 # (you would need to import sys package above)
 # name = sys.argv[1] if len(sys.argv) > 1 else "World"
 
 greeting = f"Hello, {name}! Time is {dt.datetime.now(dt.timezone.utc):%H:%M:%S} UTC."
+bye_message = f"Goodbye, {bye_name}!"
+
 print(greeting)
+print(bye_message)
 
 # We can use workflow commands from: https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions
 # For example, to emit a GitHub-Actions NOTICE annotation
@@ -19,3 +23,4 @@ print(f"::notice file=entrypoint.py,line=17::{greeting}")
 # Expose an output for downstream steps/jobs
 with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
     fh.write(f"greeting={greeting}\n")
+    fh.write(f"goodbye={bye_message}\n")
